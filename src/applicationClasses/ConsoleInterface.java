59632bd43bdba339c0a59222cd9908e405916f85
LeonZuev <loonatune@gmail.com>
2023-04-21 15:14:03 +0300

    Add cases to ConsoleInterface

--- src/applicationClasses/ConsoleInterface.java
+++ src/applicationClasses/ConsoleInterface.java
@@ -218,6 +218,12 @@ public class ConsoleInterface {
           worker.getMaterials().addAll(Arrays.asList(materials.split(",")));
           System.out.println("Materials added to worker!");
         }
+        case 10 -> {
+          manager.printProjects();
+        }
+        case  11 -> {
+          manager.printWorkers();
+        }
         case 0 -> {
           System.out.println("=== Exit program ===");
           System.exit(0);
